55255cfd33dc5e23b88211f877666e61a9b94669
Arnav Swami <arnavswami@Arnavs-MacBook-Air-2.local>
2024-02-16 23:17:28 -0800

    Updated portion of the Mission statement part of the website with better colors

--- src/components/home/Mission.jsx
+++ src/components/home/Mission.jsx
@@ -4,30 +4,52 @@ import image1 from "../../../src/public/home/houseimage.svg";
 
 const Mission = () => {
   return (
-    <div className="bg-red-200 flex w-full justify-center items-center">
+    <div className="bg-neutral-300 flex w-full justify-center items-center">
+      {/* Left empty space column with 1/12 width */}
+      <div className="w-1/12"></div>
+
       {/* Image container with 1/3 width */}
-      <div className="w-1/3">
-        <Image alt="" src={image1} className="mt-3 ml-auto mr-auto" />
+      <div className="w-1/3 mt-6 mb-6">
+        <Image alt="" src={image1} className="ml-4 mr-auto" />
       </div>
+
+      {/* Empty space column with 1/6 width */}
+      <div className="w-1/6"></div>
 
       {/* Content container with 2/3 width */}
-      <div className="w-1/3 bg-white flex flex-col justify-center items-center">
-        <div>
-          <p>
-            <span style={{ color: "blue", fontWeight: "bold" }}>
-              Mission Statement:
-            </span>{" "}
-            The mission for this group has been from the beginnging to provide a
-            more transparent way for students to get access to housing as well
-            as make it a lot less stressful. Students should be able to easily
-            find houses they find appealing without having to look through
-            RedFin or Zillow which may have a confusing interface to follow.
+      <div className="w-1/2 bg-neutral-300 flex flex-col justify-center items-center">
+        <div className="text-center">
+          <p
+            style={{
+              lineHeight: "2",
+              margin: "0",
+              marginTop: "10px",
+              marginRight: "20px",
+            }}
+          >
+            <span
+              style={{
+                color: "blue",
+                fontWeight: "bold",
+                fontFamily: "Georgia, sans-serif",
+                fontSize: "38px",
+              }}
+            >
+              OUR MISSION: <br />
+            </span>
+          </p>
+          {/* Empty space column with 1/6 width */}
+          <div className="w-1/6"></div>
+          <p style={{ lineHeight: "2", margin: "0", marginRight: "10px" }}>
+            <span style={{ fontFamily: "Arial, sans-serif", fontSize: "22px" }}>
+              This mission is to provide a transparent way for students to get
+              housing, and understand living conditions. We eliminate the
+              hurdles of confusing interfaces like Redfin or Zillow and help
+              students find student-reviewed houses without navigating confusing
+              sites.
+            </span>
           </p>
         </div>
-      </div>
-
-      <div className="w-1/3">
-        <Image alt="" src={image1} className="mt-3 ml-auto mr-auto" />
       </div>
     </div>
   );
