44015498133886303b5108bf0756e7bd5c40f95b
Cody Lin <codyjclin@gmail.com>
2024-02-20 00:16:23 -0800

    Added the 'Group to the banner

---
+++ src/components/home/Banner.jsx
@@ -0,0 +1,10 @@
+export default function Banner() {
+  return (
+    <div className="flex items-center justify-center h-20">
+      <div className="flex items-center space-x-4">
+        <div className="text-yellow-500 text-6xl font-bold">{"'"}</div>
+        <div className="text-white text-6xl font-bold">GROUP</div>
+      </div>
+    </div>
+  );
+}
