1120a32898ede2e09a3f8acfd2ce799a12e57c63
Cody Lin <codyjclin@gmail.com>
2024-02-19 23:33:02 -0800

    StarButton, Issue #32
    Had some issues with formatting and Next.js but we good now and this works and should pass everything

---
+++ src/components/listings/Favorites.jsx
@@ -0,0 +1,23 @@
+"use client";
+import React, { useState } from "react";
+import { StarIcon } from "@heroicons/react/solid";
+
+const StarButton = () => {
+  const [isFavorited, setIsFavorited] = useState(false);
+
+  const toggleFavorite = () => {
+    setIsFavorited(!isFavorited);
+  };
+
+  return (
+    <button onClick={toggleFavorite} className="focus:outline-none">
+      <StarIcon
+        className={`h-6 w-6 ${
+          isFavorited ? "text-yellow-500" : "text-red-500"
+        }`}
+      />
+    </button>
+  );
+};
+
+export default StarButton;
