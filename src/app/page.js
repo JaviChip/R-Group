7ed533c6128e972487c39dbe2463fd5e9ecb167c
Wilson Law <law@MacBook-Pro-110.lan>
2024-02-20 20:35:57 -0800

    finished pages number buttons

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import React from "react";
 import Mission from "@/components/home/Mission";
 import Notes from "@/components/home/Notes";
 import StarButton from "@/components/listings/Favorites";
-
+import BoxWithButtons from "@/components/listings/Pages";
 const Home = () => {
   return (
     <div className="w-full overflow-clip flex flex-col justify-center items-center bg-neutral-300">
@@ -10,7 +10,7 @@ const Home = () => {
         <Mission />
         <StarButton />
       </div>
-
+<BoxWithButtons/>
       <Notes />
     </div>
   );
