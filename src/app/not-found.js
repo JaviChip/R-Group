2adb188ee37fed7f169528bcecb4bcc1587771df
Wilson Law <law@MacBook-Pro-110.lan>
2024-02-20 19:51:11 -0800

    finished 404 page

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,5 +1,20 @@
-const NotFound = () => {
-  return <div>404 Page Not Found</div>;
+import React from "react";
+
+const NotFoundPage = () => {
+  return (
+    <div className="flex items-center justify-center w-full h-screen bg-gray-100">
+      <div className="text-center">
+        <h1 className="mb-5 text-6xl font-bold text-gray-800">Whoops! 404</h1>
+        <p className="mb-5 text-xl font-light text-gray-600">Page not found</p>
+        <a
+          href="/"
+          className="px-6 py-3 text-sm font-semibold text-white bg-blue-500 rounded-md hover:bg-blue-600"
+        >
+          Go Home
+        </a>
+      </div>
+    </div>
+  );
 };
 
-export default NotFound;
+export default NotFoundPage;
