89985eb90d1397ccc3cb844ae6e6d2b905997780
Javier Becerril <114547951+JaviChip@users.noreply.github.com>
2024-02-29 17:50:52 -0800

    Update page.js
    Signed-off-by: Javier Becerril <114547951+JaviChip@users.noreply.github.com>

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@ import React from "react";
 import Mission from "@/components/home/Mission";
 import Notes from "@/components/home/Notes";
 import StarButton from "@/components/listings/Favorites";
-import BoxWithButtons from "@/components/listings/Pages";
+//import BoxWithButtons from "@/components/listings/Pages";
 import ContactSellerButton from "@/components/listings/ListingAttributes/Contact";
 import NotFoundPage from "./not-found";
 const Home = () => {
