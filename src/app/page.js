c839bc5cc9e70456843179d69734d8d15884b18d
javi <javi@Javis-MacBook-Pro-3.local>
2024-02-29 18:17:55 -0800

    Switch Favorite Implementatio

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,18 @@
 import React from "react";
 import Mission from "@/components/home/Mission";
-import Notes from "@/components/home/Notes";
-import StarButton from "@/components/listings/Favorites";
+// import Notes from "@/components/home/Notes";
 // import BoxWithButtons from "@/components/listings/Pages";
 import ContactSellerButton from "@/components/listings/ListingAttributes/Contact";
 import NotFoundPage from "./not-found";
+
 const Home = () => {
   return (
     <div className="w-full overflow-clip flex flex-col justify-center items-center bg-neutral-300">
       <div className=" mt-24 mb-60">
         <Mission />
-        <StarButton />
       </div>
       <ContactSellerButton />
       <NotFoundPage />
-      <Notes />
     </div>
   );
 };
