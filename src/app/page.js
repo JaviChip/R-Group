d88861909926ad9ec2381afa5e75ccb70d20a8b3
Arnav Swami <arnavswami@Arnavs-Air-2.lan>
2024-03-04 00:09:59 -0800

    Review button changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,12 @@
 import React from "react";
 import Mission from "@/components/home/Mission";
 import Notes from "@/components/home/Notes";
-import StarButton from "@/components/listings/Favorites";
-
+import Reviews from "/Users/arnavswami/Documents/Arnav/R-Group/src/components/listings/ListingAttributes/Reviews";
 const Home = () => {
   return (
     <div className="w-full overflow-clip flex flex-col justify-center items-center bg-neutral-300">
       <div className=" mt-24 mb-60">
         <Mission />
-        <StarButton />
       </div>
 
       <Notes />
