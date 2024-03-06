f21ae96c7ab0c111ec87d20863cfa53aa3a97623
Cody Lin <codyjclin@gmail.com>
2024-03-06 12:41:29 -0800

    Made it so we are grabbing data from mock data

--- src/components/listings/ListingAttributes/Contact.jsx
+++ src/components/listings/ListingAttributes/Contact.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 import { PhoneIncomingIcon } from "@heroicons/react/solid";
 
-const ContactSellerButton = () => {
+const ContactSellerButton = ({ Phone }) => {
   return (
     <button className="flex flex-none bg-green-600 hover:bg-green-700 text-white font-bold py-2 px-2 rounded-xl">
       <div className="flex flex-row flex-nowrap items-center">
         <PhoneIncomingIcon className="h-8 px-1" />
-        <div className=" flex flex-nowrap ml-2 pr-2">Contact</div>
+        <div className=" flex flex-nowrap ml-2 pr-2">Contact - {Phone}</div>
       </div>
     </button>
   );
