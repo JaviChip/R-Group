b0405c572b938bdf58a81b82f9d59475e00f41a2
Arnav Swami <arnavswami@ucr-secure-32-10-13-179-12.wnet.ucr.edu>
2024-03-06 15:37:59 -0800

    Reviews button updated to link to listings page

---
+++ src/components/listings/ListingAttributes/Reviews.jsx
@@ -0,0 +1,21 @@
+"use client";
+import React from "react";
+import Link from "next/link";
+import { StarIcon } from "@heroicons/react/solid";
+
+const Reviews = () => {
+  return (
+    <div className="bg-black p-3 inline-flex items-center space-x-1">
+      <span className="text-white font-bold">Reviews</span>
+      <Link href="/reviews">
+        <div className="flex focus:outline-none">
+          {[...Array(5)].map((_, index) => (
+            <StarIcon key={index} className="h-6 w-6 text-red-500" />
+          ))}
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default Reviews;
