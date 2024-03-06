9ac27197bbc73ec365c5ad5564bdafb1f83df2a2
Arnav Swami <arnavswami@ucr-secure-32-10-13-179-12.wnet.ucr.edu>
2024-03-06 14:17:12 -0800

    Now Reviews button links to reviews page where the student can see the reviews and also leave a review if they woudl wish.

--- src/components/listings/ListingAttributes/Reviews.jsx
+++ src/components/listings/ListingAttributes/Reviews.jsx
@@ -7,7 +7,7 @@ const Reviews = () => {
   return (
     <div className="bg-black p-3 inline-flex items-center space-x-1">
       <span className="text-white font-bold">Reviews</span>
-      <Link href="/listings">
+      <Link href="/reviews">
         <div className="flex focus:outline-none">
           {[...Array(5)].map((_, index) => (
             <StarIcon key={index} className="h-6 w-6 text-red-500" />
